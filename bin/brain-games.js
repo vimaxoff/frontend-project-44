b81ecdf1dc5076854d380cdf42a7469727f463a8
Vladimir Maksimov <vimaximoff@yandex.ru>
2022-12-12 22:40:59 +1100

    add hallo function

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
+import sayHallo from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
+sayHallo();
---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const sayHallo = () => {
+   const name = readlineSync.question('May I have your name? ');
+   console.log(`Hallo, ${name}!`);
+};
+
+export default sayHallo;
