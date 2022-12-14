90e41d67f50e3be54458c057dcd0c4dce5561886
Vladimir Maksimov <vimaximoff@yandex.ru>
2022-12-14 21:47:11 +1100

    add eslint

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const sayHallo = () => {
-   const name = readlineSync.question('May I have your name? ');
-   console.log(`Hallo, ${name}!`);
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hallo, ${name}!`);
 };
 
 export default sayHallo;
