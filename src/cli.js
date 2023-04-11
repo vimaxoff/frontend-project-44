a5f7df55b8f0fd4fac0c9b216a5920e28cb2469b
Vladimir Maksimov <vimaximoff@yandex.ru>
2023-04-12 00:24:49 +1100

    fix the code

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
 
-const sayHallo = () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hallo, ${name}!`);
+const sayHello = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  return userName;
 };
 
-export default sayHallo;
+export default sayHello;
