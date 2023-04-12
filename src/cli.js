d57a32b15614f2607fc525a456f160ae9600b7f3
Vladimir Maksimov <vimaximoff@yandex.ru>
2023-04-12 21:13:22 +1100

    fix problems

--- src/cli.js
+++ src/cli.js
@@ -3,8 +3,7 @@ import readlineSync from 'readline-sync';
 const sayHello = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  return userName;
+  return console.log(`Hello, ${userName}!`);
 };
 
 export default sayHello;
